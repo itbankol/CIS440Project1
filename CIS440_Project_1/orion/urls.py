a43a60c850d10a7cda3c8cced46c169aa1398b24
khinsey <63768293+khinsey@users.noreply.github.com>
2021-09-19 14:30:21 -0700

    User Registration
    Now able to create a new user from an HTML page. The user will be saved in the database

--- CIS440_Project_1/orion/urls.py
+++ CIS440_Project_1/orion/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='orion-home'),
-    path('create-user/', views.create_user, name='orion-create-user')
+    path('user-registration/', views.user_registration, name='orion-user-registration')
 ]
--- CIS440_Project_1/orion/views.py
+++ CIS440_Project_1/orion/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+# from django.http import HttpResponse
+from django.contrib import messages
+from .forms import RegisterUserForm
 
-# Create your views here.
 def home(request):
     return render(request, 'orion/index.html')
 
 
-def create_user(request):
-    return render(request, 'orion/create_user.html')
+def user_registration(request):
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/') # the parameter inside the redirect method takes a string of a Django 'views' name
+    else:
+        form = RegisterUserForm()
+
+    return render(request, 'orion/user_registration.html', {'form': form})
