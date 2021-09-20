b9c683ee896c870985f2d4c82eaae1b94602a957
benjaminostler <benjaminostler@gmail.com>
2021-09-20 11:52:15 -0700

    css, js, html, Buttons, & Table created for home page
    -I created buttons linked to functions in the index.js for each mode we want the user or staff member to be able to enter and a separate HTML file for each as well:
      1) creating users
      2) creating events
      3) editing events (edit or delete)
      4) editing users (edit, delete, or blacklist)
      5) manage blacklist (add or delete users from blacklist)
      - Inside each html file I created a table with 2 cells for formatting:
      forms in left cell, list(s) in right cell.
    -I created a div inside the body, below the buttons as a window for plugging in the various modes when coded. The idea is when a button is clicked whatever is currently in the window will be cleared and the new mode will be inserted instead.

---
+++ CIS440_Project_1/orion/templates/orion/index.js
@@ -0,0 +1,53 @@
+"use strict";
+
+var createUserButton, editUserButton, deleteUserButton, blacklistUserButton;
+var createEventButton, editEventButton, deleteEventButton;
+
+function initialize() {
+    createUserButton = document.getElementById("createUserId");
+    editUserButton = document.getElementById("editUserId"); 
+    deleteUserButton = document.getElementById("deleteUserId"); 
+    blacklistUserButton = document.getElementById("blacklistUserId"); 
+    createEventButton = document.getElementById("createEventId"); 
+    editEventButton = document.getElementById("editEventId"); 
+    deleteEventButton = document.getElementById("deleteEventId");      
+    
+} // end of initialize()
+
+//User Management Functions
+function create_user() {
+    // creates user
+
+}
+
+function edit_user() {
+    // edits user
+
+}
+
+function delete_user() {
+    // deletes user
+
+}
+
+function blacklist_user() {
+    // blacklists user
+
+}
+
+
+// Event Management Functions
+function create_event() {
+    // creates event
+    
+}
+
+function edit_event() {
+    // edits event
+
+}
+
+function delete_event() {
+    // deletes event
+
+}
