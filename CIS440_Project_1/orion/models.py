6e8065da629233239b62ef22b8a99d4d49dcbf66
khinsey <63768293+khinsey@users.noreply.github.com>
2021-09-19 20:59:17 -0700

    Database Migrations for Event
    Made a model in models.py for events. Then migrated to the database

--- CIS440_Project_1/orion/models.py
+++ CIS440_Project_1/orion/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    date_of_event = models.DateTimeField()
+    time_of_event = models.TimeField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE) # on_delete=CASCADE --> when a user is deleted, all of their events will be deleted as well
