af167c4064c2931eaab9a14be519aa71cd5d81d1
benjaminostler <benjaminostler@gmail.com>
2021-09-20 12:35:13 -0700

    Created HTML's, JS, CSS, and added buttons, Table, and functions

---
+++ CIS440_Project_1/orion/templates/orion/index.js
@@ -0,0 +1,81 @@
+"use strict";
+
+var createUserButton, editUserButton, deleteUserButton, blacklistUserButton;
+var createEventButton, editEventButton, deleteEventButton;
+
+function initialize() {
+    createUserButton = document.getElementById("createUserId");
+    editUserButton = document.getElementById("editUserId");
+    createEventButton = document.getElementById("createEventId"); 
+    editEventButton = document.getElementById("editEventId");
+    manageBlacklistingButton = document.getElementById("blacklistingId");
+    blacklistButton = document.getElementById("blacklistId");     
+} // end of initialize()
+
+//User Management Functions
+function create_user() {
+    // creates user
+
+}
+
+function save_user() {
+    // saves user
+
+}
+
+function edit_user() {
+    // edits user
+
+}
+
+function delete_user() {
+    // deletes user
+
+}
+
+function delete_user_draft() {
+    // deletes user draft
+
+}
+
+function blacklist() {
+    // adds user to blacklist
+
+}
+
+function blacklisting() {
+    // manage blacklisting
+
+}
+
+function remove_from_blacklist() {
+    // remove user from blacklist
+
+}
+
+// Event Management Functions
+function create_event() {
+    // creates event
+    
+}
+
+function save_event() {
+    // saves event
+    
+}
+
+function edit_event() {
+    // edits event
+
+}
+
+function delete_event() {
+    // deletes event
+
+}
+
+function delete_event_draft() {
+    // deletes event draft
+
+}
+
