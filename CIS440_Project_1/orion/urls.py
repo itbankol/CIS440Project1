faa47b6b416957f36c25a4cc71cb8c832f12a175
khinsey <63768293+khinsey@users.noreply.github.com>
2021-09-27 09:43:35 -0700

    Updates

--- CIS440_Project_1/orion/urls.py
+++ CIS440_Project_1/orion/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('user-registration/', views.user_registration, name='user-registration'),
     path('create-new-event/', views.create_new_event, name='create-new-event'),
     path('blacklist/', views.blacklist, name='blacklist'),
-    path('edit_event/', views.edit_event, name='edit_event'),
-    path('edit_user/', views.edit_user, name='edit_user'),
+    path('edit-event/', views.edit_event, name='edit-event'),
+    path('edit-user/', views.edit_user, name='edit-user'),
 ]
