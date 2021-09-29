4fed92047e8373087c3e347d6f13cb431b4a6175
khinsey <63768293+khinsey@users.noreply.github.com>
2021-09-28 20:55:40 -0700

    Update

--- CIS440_Project_1/orion/models.py
+++ CIS440_Project_1/orion/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Event(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    attendees = models.TextField(default='') # will be a JSON string 
+    attendees = models.TextField(default='')  
     date_of_event = models.DateTimeField()
     author = models.ForeignKey(User, on_delete=models.CASCADE) # on_delete=CASCADE --> when a user is deleted, all of their events will be deleted as well
     
